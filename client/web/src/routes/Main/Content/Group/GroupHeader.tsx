72578953fdc6a38ee1866b3667e92456edd5f00d
xiao8wei8 <xiao8wei8@163.com>
2023-07-29 10:01:54 +0800

    fix: 看板工具和私信

--- client/web/src/routes/Main/Content/Group/GroupHeader.tsx
+++ client/web/src/routes/Main/Content/Group/GroupHeader.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import type { MenuProps } from 'antd';
 import _isNil from 'lodash/isNil';
 import _compact from 'lodash/compact';
 import {
   PERMISSION,
+  useAppSelector,
   useGroupInfo,
   useHasGroupPermission,
   useTranslation,
 } from 'tailchat-shared';
 import { SectionHeader } from '@/components/SectionHeader';
 import { useGroupHeaderAction } from './useGroupHeaderAction';
+import { Icon } from 'tailchat-design';
+import { IconBtn } from '@/components/IconBtn';
+import { openModal } from '@/components/Modal';
+import { UserListItem } from '@/components/UserListItem';
+import { FriendList } from './FriendList';
 
 interface GroupHeaderProps {
   groupId: string;
@@ -52,9 +58,65 @@ export const GroupHeader: React.FC<GroupHeaderProps> = React.memo((props) => {
   };
 
   return (
-    <SectionHeader menu={menu} data-testid="group-header">
-      {groupInfo?.name}
-    </SectionHeader>
+    <div
+      style={{
+        display: 'flex',
+        gap: '10px',
+      }}
+    >
+      <div>
+        <SectionHeader menu={menu} data-testid="group-header">
+          {groupInfo?.name}
+        </SectionHeader>
+      </div>
+
+      <div
+        style={{
+          alignItems: 'center',
+          flex: 1,
+          justifyContent: 'flex-end',
+
+          display: 'grid',
+
+          paddingRight: '15px',
+        }}
+      >
+        <IconBtn
+          key="create"
+          title={t('创建会话')}
+          shape="square"
+          icon="nimbus:edit"
+          iconClassName="text-2xl"
+          onClick={() =>
+            openModal(
+              // <div>hello</div>
+              // <CreateDMConverse hiddenUserIds={converse.members} />
+              <ConversePanelMembers members={[]} groupId={groupId} />
+            )
+          }
+        />
+      </div>
+    </div>
   );
 });
 GroupHeader.displayName = 'GroupHeader';
+
+const ConversePanelMembers: React.FC<{ members: string[]; groupId: string }> =
+  React.memo(({ members, groupId }) => {
+    return <FriendList groupId={groupId} onSwitchToAddFriend={() => {}} />;
+    const friendIds = useAppSelector(
+      (state) => state.user.friends.map((f) => f.id)
+      // .filter((item) => !withoutUserIds.includes(item))
+    );
+
+    console.log('[FriendPicker]', friendIds);
+
+    return (
+      <div>
+        {friendIds.map((member) => (
+          <UserListItem key={member} userId={member} />
+        ))}
+      </div>
+    );
+  });
+ConversePanelMembers.displayName = 'ConversePanelMembers';
